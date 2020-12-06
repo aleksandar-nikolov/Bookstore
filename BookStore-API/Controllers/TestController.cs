83c15fa236728f3456880de3f5595bfbb626b384
Aleksandar Nikolov <aleksandar.nikolov@zuhlke.com>
2020-12-06 14:19:43 +0200

    Just fixes

---
+++ BookStore-API/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore_API.Mappings.DTOs;
+using Microsoft.AspNetCore.Http;
+
+namespace BookStore_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpGet]
+        public async Task<IActionResult> GetBooks()
+        {
+            try
+            {
+                return Ok(new { result = "Test OK" });
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Test not OK");
+            }
+        }
+    }
+}
